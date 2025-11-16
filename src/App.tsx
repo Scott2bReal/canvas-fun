5912b3661bb0b177026bf0c5213f19a2d40f3473
Scott Hoecker <hoeckers@gmail.com>
2025-11-16 13:20:41 -0600

    Change font import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { MotionConfig } from "motion/react"
 import { CursorStoreProvider } from "./components/CursorStoreProvider"
 import { CursorTrail } from "./components/CursorTrail"
 import { WindowSizeProvider } from "./components/WindowSizeProvider"
-import "./styles/index.css"
 
 function App() {
   return (
